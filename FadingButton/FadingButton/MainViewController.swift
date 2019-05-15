4aaa61d36dce7e27c121b274b392361b5a460eb6
Jonathan Lehr <jonathan@aboutobjects.com>
2019-05-15 09:23:08 -0400

    Initial commit of FadingButton proj

---
+++ FadingButton/FadingButton/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  FadingButton
+//
+//  Created by Jonathan Lehr on 5/15/19.
+//  Copyright © 2019 About Objects. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var goodbyeLabel: UILabel!
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    @IBAction func startAnimating() {
+        UIView.animateKeyframes(withDuration: 1.25,
+                                delay: 0,
+                                options: [.repeat, .autoreverse],
+                                animations: { self.configureFadeAnimation() },
+                                completion: {_ in })
+    }
+    
+    func configureFadeAnimation() {
+        UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.5) {
+            self.helloLabel.alpha = 0
+        }
+        UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
+            self.goodbyeLabel.alpha = 1
+        }
+    }
+}
+
