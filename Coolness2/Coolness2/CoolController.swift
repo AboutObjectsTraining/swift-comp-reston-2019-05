59d8ed076dcee1283ae5880eb585e41d2634a566
Jonathan Lehr <jonathan@aboutobjects.com>
2019-05-15 13:32:07 -0400

    Nib file loading

---
+++ Coolness2/Coolness2/CoolController.swift
@@ -0,0 +1,99 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.a
+// See LICENSE.txt for this project's licensing information.
+
+import UIKit
+
+extension CoolController: UITextFieldDelegate
+{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("In \(#function)")
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+class CoolController: UIViewController
+{
+    static let coolFieldNib = UINib(nibName: "CoolField", bundle: nil)
+    
+    var contentView: UIView!
+    var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // loadTextFields()
+    }
+    
+    private func loadTextFields() {
+        for index in 1...3 {
+            guard
+                let topLevelObjs = CoolController.coolFieldNib.instantiate(withOwner: nil, options: nil) as? [UITextField],
+                let field = topLevelObjs.first
+                else { fatalError("CoolField nib doesn't contain a textfield") }
+            view.addSubview(field)
+            field.frame.origin.y = CGFloat(index * 60)
+        }
+    }
+    
+    @objc func addCell() {
+        print("In \(#function)")
+        let newCell = CoolViewCell()
+        newCell.text = textField.text
+        contentView.addSubview(newCell)
+    }
+    
+    private func loadView3() {
+        Bundle.main.loadNibNamed("CoolStuff", owner: self, options: nil)
+    }
+    
+    private func loadView2() {
+        guard let topLevelObjs = Bundle.main.loadNibNamed("CoolStuff", owner: nil, options: nil) else { return }
+        view = topLevelObjs.first as? UIView
+    }
+    
+    private func loadView1() {
+        view = UIView()
+        view.backgroundColor = UIColor.brown
+        
+        let (accessoryRect, contentRect) = UIScreen.main.bounds.divided(atDistance: 90, from: .minYEdge)
+        let accessoryView = UIView(frame: accessoryRect)
+        contentView = UIView(frame: contentRect)
+        view.addSubview(accessoryView)
+        view.addSubview(contentView)
+        
+        accessoryView.backgroundColor = UIColor(white: 1.0, alpha: 0.7)
+        contentView.backgroundColor = UIColor(white: 1.0, alpha: 0.5)
+        contentView.clipsToBounds = true
+
+        // Controls
+        
+        textField = UITextField(frame: CGRect(x: 15, y: 52, width: 240, height: 30))
+        accessoryView.addSubview(textField)
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Enter a label"
+        
+        textField.delegate = self
+        
+        let button = UIButton(type: .system)
+        accessoryView.addSubview(button)
+        button.setTitle("Add", for: .normal)
+        button.sizeToFit()
+        button.frame = button.frame.offsetBy(dx: 270, dy: 52)
+        
+        button.addTarget(self, action: #selector(addCell), for: .touchUpInside)
+        
+        // Cells
+        let subview1 = CoolViewCell(frame: CGRect(x: 20, y: 60, width: 200, height: 40))
+        let subview2 = CoolViewCell(frame: CGRect(x: 40, y: 120, width: 200, height: 40))
+        
+        contentView.addSubview(subview1)
+        contentView.addSubview(subview2)
+        
+        subview1.text = "Hello 🌏🌎👻!"
+        subview2.text = "CoolViewCells rock! 🍾🎉"
+        
+        subview1.backgroundColor = UIColor.purple
+        subview2.backgroundColor = UIColor.orange
+    }
+}
+
---
+++ Coolness2/Coolness2/CoolViewCell.swift
@@ -0,0 +1,130 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+import UIKit
+
+private let textInsets = UIEdgeInsets(top: 7, left: 12, bottom: 8, right: 12)
+
+private let textAttributes: [NSAttributedString.Key: Any] = [
+    .font: UIFont.boldSystemFont(ofSize: 20),
+    .foregroundColor: UIColor.white
+]
+
+extension UIEdgeInsets
+{
+    var origin: CGPoint { return CGPoint(x: left, y: top) }
+    var width: CGFloat { return left + right }
+    var height: CGFloat { return top + bottom }
+}
+
+class CoolViewCell: UIView
+{
+    var highlighted = false {
+        didSet { alpha = highlighted ? 0.5 : 1.0 }
+    }
+    
+    var text: String? {
+        didSet { sizeToFit() }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureLayer()
+        configureGestureRecognizers()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        // FIXME: Don't crash
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureGestureRecognizers() {
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(bounce))
+        recognizer.numberOfTapsRequired = 2
+        addGestureRecognizer(recognizer)
+    }
+    
+    private func configureLayer() {
+        layer.borderWidth = 3
+        layer.borderColor = UIColor.white.cgColor
+        layer.cornerRadius = 8
+        layer.masksToBounds = true
+    }
+}
+
+// MARK: - Animation
+
+extension CoolViewCell
+{
+    @objc private func bounce() {
+        print("In \(#function)")
+        // TODO: Pull out constant values
+        animateBounce(duration: 1, size: CGSize(width: 120, height: 240))
+    }
+    
+    private func configureBounce(size: CGSize) {
+        UIView.setAnimationRepeatCount(3.5)
+        UIView.setAnimationRepeatAutoreverses(true)
+        let translation = CGAffineTransform(translationX: size.width, y: size.height)
+        self.transform = translation.rotated(by: .pi / 2)
+    }
+    
+    fileprivate func animateFinalBounce(duration: TimeInterval) {
+        UIView.animate(withDuration: duration) { [weak self] in
+            self?.transform = .identity
+        }
+    }
+    
+    private func animateBounce(duration: TimeInterval, size: CGSize) {
+        UIView.animate(withDuration: duration,
+                       animations: { [weak self] in self?.configureBounce(size: size) },
+                       completion: { [weak self] _ in self?.animateFinalBounce(duration: duration) })
+    }
+}
+
+// MARK: - Drawing and resizing
+
+extension CoolViewCell
+{
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        guard let text = text else { return size }
+        var newSize = (text as NSString).size(withAttributes: textAttributes)
+        newSize.width += textInsets.width
+        newSize.height += textInsets.height
+        return newSize
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let text = text else { return }
+        (text as NSString).draw(at: textInsets.origin, withAttributes: textAttributes)
+    }
+}
+
+
+// MARK: - UIResponder methods
+
+extension CoolViewCell
+{
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        superview?.bringSubviewToFront(self)
+        highlighted = true
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let currLocation = touch.location(in: nil)
+        let prevLocation = touch.previousLocation(in: nil)
+        
+        center.x += currLocation.x - prevLocation.x
+        center.y += currLocation.y - prevLocation.y
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        highlighted = false
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        highlighted = false
+    }
+}
